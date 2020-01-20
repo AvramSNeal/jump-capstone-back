dc1bb4dcf811059ea4ed5c2e541d832160271991
Oscar Holloway <aoholloway@hotmail.com>
2020-01-20 13:47:05 -0600

    test commit

--- capstone/src/main/java/com/cognixia/project/CapstoneApplication.java
+++ capstone/src/main/java/com/cognixia/project/CapstoneApplication.java
@@ -8,13 +8,11 @@ public class CapstoneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneApplication.class, args);
-	}
-<<<<<<< HEAD
-=======
-	
+	}	
 	// Thomas White will NEVER find me! Ha ha
 	
 	// Robert was here
+	
+	// A wild Oscar appeared!
 
->>>>>>> a249a838b9f1bf88871e889f481e1e047a506caa
 }
