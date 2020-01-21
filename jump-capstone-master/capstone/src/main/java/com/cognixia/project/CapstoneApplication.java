4f6e3bd48a70b013cf9b4ee593453252b307ca2e
ryantrot <51455606+ryantrot@users.noreply.github.com>
2020-01-21 11:24:34 -0600

    Add files via upload
    Updated TodoController and Todo files

---
+++ jump-capstone-master/capstone/src/main/java/com/cognixia/project/CapstoneApplication.java
@@ -0,0 +1,15 @@
+package com.cognixia.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class CapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneApplication.class, args);
+	}
+
+}
---
+++ jump-capstone-master/capstone/src/main/java/com/cognixia/project/dao/TodoDAO.java
@@ -0,0 +1,38 @@
+package com.cognixia.project.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognixia.project.model.Todo;
+import com.cognixia.project.repository.TodoRepository;
+
+@Service
+public class TodoDAO {
+	
+	@Autowired
+	TodoRepository repository;
+	
+	// SAVE TODO 
+	public Todo save(Todo todo) {
+		return repository.saveAndFlush(todo);
+	}
+	
+	// FIND ALL TODOS
+	public List<Todo> findAll() {
+		return repository.findAll();
+	}
+	
+	// FIND TODO BY ID
+	public Optional<Todo> findById(Long id) {
+		return repository.findById(id);
+	}
+	
+	// DELETE TODO
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
+
+}
