07b208d529fb8043dac72a28e02dcdd286ff1e47
Shinigami <Shinigami@DESKTOP-95OD524>
2020-01-22 19:58:37 -0600

    Schema and data files updated

--- capstone/target/classes/data.sql
+++ capstone/target/classes/data.sql
@@ -1,14 +1,28 @@
-INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status)
-VALUES("clean up the barracks", "ACTIVE", CURDATE(), 1, 1);
-INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status)
-VALUES("clean up your bunk", "ACTIVE", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Start roll call of active duty soldiers", "OFFICER", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Check training to ensure standards are established", "OFFICER", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Advise both commanders and first sergeants on their tasks", "OFFICER", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Monitor the progress of active duty soldiers", "OFFICER", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Report to army reserve professional", "OFFICER", CURDATE(), 1, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Command active duty soldiers to perform routine maintenance", "OFFICER", CURDATE(), 3, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Perform evaluations on active duty soldiers", "OFFICER", CURDATE(), 3, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Take roll call and dismiss the unit for supper", "OFFICER", CURDATE(), 2, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Give daily report to army reserve professional", "OFFICER", CURDATE(), 1, 1);
 
-INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status)
-VALUES("pick up trash", "RESERVE", CURDATE(), 3, 1);
-INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status)
-VALUES("complete your physical training", "RESERVE", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Attend daily briefing", "RESERVE", CURDATE(), 1, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Submit morning logs of brief meeting", "RESERVE", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Visit with army officials and their units", "RESERVE", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Receive report from army official", "RESERVE", CURDATE(), 1, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Submit afternoon logs of report", "RESERVE", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Perform evaluations on army officer", "RESERVE", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Submit final report for the day", "RESERVE", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Attend evening debriefing", "RESERVE", CURDATE(), 1, 1);
 
-INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status)
-VALUES("complete scheduling paperwork", "OFFICER", CURDATE(), 1, 1);
-INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status)
-VALUES("complete the todo tasks for reserves", "OFFICER", CURDATE(), 3, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Attend roll call", "ACTIVE", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Do daily training with unit", "ACTIVE", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Practice standard procedures and processes", "ACTIVE", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Do a sweep of the perimeter", "ACTIVE", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Report to army officer", "ACTIVE", CURDATE(), 1, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Perform routine maintenance", "ACTIVE", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Attend daily evaluations", "ACTIVE", CURDATE(), 3, 0);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Attend roll call and go to supper", "ACTIVE", CURDATE(), 2, 1);
+INSERT INTO todo_table (todo_description, todo_user, todo_date, todo_priority, todo_status) VALUES("Give daily report to army officer", "ACTIVE", CURDATE(), 1, 1);
